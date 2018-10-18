662bb1295e7d68cf2bd00329365102cfe71b9d2b
brinkqiang <brink.qiang@gmail.com>
2018-10-18 10:59:08 +0800

    ADD

--- include/dmloopbuffer.h
+++ include/dmloopbuffer.h
@@ -1,29 +1,27 @@
 
-// Copyright (c) 2018 brinkqiang (brink.qiang@gmail.com)
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
-
 #ifndef __DMLOOPBUFFER_H_INCLUDE__
 #define __DMLOOPBUFFER_H_INCLUDE__
 
-#include "dmos.h"
+#include "dmtypes.h"
 
-int DMAPI DMLOOPBUFFER_MAIN( int argc, char* argv[] );
+class CDMLoopBuffer {
+  public:
+    CDMLoopBuffer(void);
+    ~CDMLoopBuffer(void);
+
+    bool Init(int32_t nSize);
+
+    bool PushBack(const char* pData, int32_t nLen);
+
+    bool PopFront(char* pBuf, int32_t nLen);
+
+    void DiscardFront(int32_t nLen);
+
+  protected:
+    char* m_pBuffer;
+    char* m_pHead;
+    char* m_pTail;
+    int32_t m_nSize;
+};
 
 #endif // __DMLOOPBUFFER_H_INCLUDE__
--- src/dmloopbuffer.cpp
+++ src/dmloopbuffer.cpp
@@ -1,26 +1,100 @@
-
-// Copyright (c) 2018 brinkqiang (brink.qiang@gmail.com)
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
-
+#include <stdio.h>
+#include <string.h>
 #include "dmloopbuffer.h"
 
-int DMAPI DMLOOPBUFFER_MAIN( int argc, char* argv[] ){
-    return -1;
+
+CDMLoopBuffer::CDMLoopBuffer(void)
+{
+    m_pBuffer = NULL;
+    m_pHead = NULL;
+    m_pTail = NULL;
+    m_nSize = 0;
+}
+
+CDMLoopBuffer::~CDMLoopBuffer(void)
+{
+    if (m_pBuffer != NULL)
+        delete m_pBuffer;
+}
+
+bool CDMLoopBuffer::Init(int32_t nSize)
+{
+    m_pBuffer = new char[nSize];
+    if (NULL == m_pBuffer)
+        return false;
+
+    m_pHead = m_pBuffer;
+    m_pTail = m_pBuffer;
+    m_nSize = nSize;
+
+    return true;
+}
+
+bool CDMLoopBuffer::PushBack(const char* pData, int32_t nLen)
+{
+    int32_t nDist = (int32_t)(m_pTail + (uint32_t)m_nSize - m_pHead);
+    int32_t nUsed = nDist >= m_nSize ? (nDist - m_nSize) : nDist;
+    if (nLen + nUsed + 1 > m_nSize)
+        return false;
+
+    if (m_pTail + nLen >= m_pBuffer + m_nSize)
+    {
+        int32_t nSeg1 = (int32_t)(m_pBuffer + m_nSize - m_pTail);
+        int32_t nSeg2 = nLen - nSeg1;
+        memcpy(m_pTail, pData, nSeg1);
+        memcpy(m_pBuffer, pData + nSeg1, nSeg2);
+        m_pTail = m_pBuffer + nSeg2;
+    }
+    else
+    {
+        memcpy(m_pTail, pData, nLen);
+        m_pTail += nLen;
+    }
+
+    return true;
+}
+
+bool CDMLoopBuffer::PopFront(char* pBuf, int32_t nLen)
+{
+    int32_t nDist = (int32_t)(m_pTail + (uint32_t)m_nSize - m_pHead);
+    int32_t nUsed = nDist >= m_nSize ? (nDist - m_nSize) : nDist;
+
+    if (nLen > nUsed)
+        return false;
+
+    if (m_pHead + nLen >= m_pBuffer + m_nSize)
+    {
+        int32_t nSeg1 = (int32_t)(m_pBuffer + m_nSize - m_pHead);
+        int32_t nSeg2 = nLen - nSeg1;
+        memcpy(pBuf, m_pHead, nSeg1);
+        memcpy(pBuf + nSeg1, m_pBuffer, nSeg2);
+        m_pHead = m_pBuffer + nSeg2;
+    }
+    else
+    {
+        memcpy(pBuf, m_pHead, nLen);
+        m_pHead += nLen;
+    }
+
+    return true;
+}
+
+void CDMLoopBuffer::DiscardFront(int32_t nLen)
+{
+    int32_t nDist = (int32_t)(m_pTail + (uint32_t)m_nSize - m_pHead);
+    int32_t nUsed = nDist >= m_nSize ? (nDist - m_nSize) : nDist;
+
+    if (nLen > nUsed)
+        return;
+
+    if (m_pHead + nLen >= m_pBuffer + m_nSize)
+    {
+        int32_t nSeg1 = (int32_t)(m_pBuffer + m_nSize - m_pHead);
+        int32_t nSeg2 = nLen - nSeg1;
+        m_pHead = m_pBuffer + nSeg2;
+    }
+    else
+    {
+        m_pHead += nLen;
+    }
 }
