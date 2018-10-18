04c49655689069088ab3705d65e0888da90a553b
brinkqiang <brink.qiang@gmail.com>
2018-10-18 11:06:53 +0800

    add

--- test/dmloopbuffertest.cpp
+++ test/dmloopbuffertest.cpp
@@ -1,8 +1,23 @@
 
 #include "dmloopbuffer.h"
+#include "dmos.h"
 #include "dmformat.h"
+#include <iostream>
 
 int main( int argc, char* argv[] ) {
-    std::cout << fmt::format("{0} {1}", "hello world! ", DMLOOPBUFFER_MAIN(argc, argv));
+    CDMLoopBuffer oBuffer;
+    oBuffer.Init(1024 * 512);
+    
+    std::string strData = "hello world";
+
+    while (!oBuffer.PushBack(strData.c_str(), strData.size()))
+    {
+        SleepMs(1);
+    }
+
+    std::string strData2;
+    strData2.resize(strData.size());
+    oBuffer.PopFront(strData2.data(), strData2.size());
+
     return 0;
 }
